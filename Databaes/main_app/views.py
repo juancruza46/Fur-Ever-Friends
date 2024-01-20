9a16d320fc98d25289cb8759a29a47e2757cb7c4
Ryan Luis <RyanCLuis@gmail.com>
2024-01-20 09:48:53 -0800

    Adds detail page

--- Databaes/main_app/views.py
+++ Databaes/main_app/views.py
@@ -22,3 +22,7 @@ def dogs_index(request):
     dogs = Dog.objects.all()
     return render (request, 'dogs/index.html', {'dogs': dogs})
 
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', { 'dog': dog })
+
