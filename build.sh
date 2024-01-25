bcd8d9e272439eefc6f026217952826fb44c1e9d
Ryan Luis <RyanCLuis@gmail.com>
2024-01-25 13:19:05 -0800

    Deploying

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ pip install --upgrade pip
 pip install --force-reinstall -U setuptools
 pip install django
 pip install dj_database_url
+pip install environ
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
