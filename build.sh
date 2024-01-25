f3e5cce1ac807c82e39e3690d345f24ba8548383
Ryan Luis <RyanCLuis@gmail.com>
2024-01-25 13:28:48 -0800

    adds more into build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ pip install django
 pip install dj_database_url
 pip install django-environ
 pip install psycopg2
+pip install boto3
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
