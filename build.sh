00a253c73508296e63e7897f87a295b5ee141062
Ryan Luis <RyanCLuis@gmail.com>
2024-01-25 13:24:25 -0800

    Deploying

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ pip install --force-reinstall -U setuptools
 pip install django
 pip install dj_database_url
 pip install django-environ
+pip install psycopg2
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
