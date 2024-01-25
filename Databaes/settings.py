3288b6710f8e70022eebe78abb632d7cabc5ac8e
Ryan Luis <RyanCLuis@gmail.com>
2024-01-25 12:09:42 -0800

    deploying issues

--- Databaes/settings.py
+++ Databaes/settings.py
@@ -23,6 +23,7 @@ environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+pip install django
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
